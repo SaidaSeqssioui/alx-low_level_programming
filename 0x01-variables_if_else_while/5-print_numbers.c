a1b38c52d28742c61b247e2f03e3de8a6928feec
SaidaSeqssioui <sseqssioui@gmail.com>
2023-04-21 15:32:37 -0700

    print numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - main block
- * Description: Print all numbers of base 10, starting from 0, followed by a new line.
+ * Description: Print all numbers of base 10, starting from 0
+ * followed by a new line.
  * Return: 0
  */
 int main(void)
